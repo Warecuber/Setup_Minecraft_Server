8b6b1f6ebe68786b9321e032bbcab89f4799e1d3
warecuber <warecuber@gmail.com>
2021-09-23 16:48:16 -0500

    Broke scrupt up into functions and started creating logic to determine version

--- setup.sh
+++ setup.sh
@@ -3,55 +3,115 @@
 MC_USER_HOME_DIR="/home/minecraft"
 MC_SERVER_DIR="/opt/minecraft/server"
 JAVA_HOME="/opt/jdk-16.0.2"
+UNAME=$(uname)
+
+#############
+# Functions #
+#############
 
 # Install packages
-sudo dnf update -y
-sudo dnf install epel-release wget -y
-sudo dnf install htop glances neofetch nano tmux -y
+installPackagesForCentOS() {
+  sudo dnf update -y
+  sudo dnf install epel-release wget -y
+  sudo dnf install htop glances neofetch nano tmux -y
+}
+installPackagesForOracleLinux() {
+  sudo dnf update -y
+  sudo dnf install oracle-epel-release-el8 wget -y
+  sudo dnf install htop glances neofetch nano tmux -y
+}
+installPackagesForUbuntu() {
+  sudo apt-get update -y
+  sudo apt-get install wget htop glances neofetch nano tmux -y
+}
 
-# Open port 25565 on the firewall
-sudo firewall-cmd --permanent --zone=public --add-port=25565/tcp
-sudo firewall-cmd --reload
+# Set firewall rules
+setFirewallRulesRHEL() {
+  sudo firewall-cmd --permanent --zone=public --add-port=25565/tcp
+  sudo firewall-cmd --reload
 
-# Create new user and groups
-sudo useradd minecraft
-sudo groupadd mc_server_admin
-sudo usermod -aG mc_server_admin minecraft
-sudo usermod -aG mc_server_admin $USER #adds current user to Minecraft admin group
+}
+setFirewallRulesUbuntu() {
+  sudo ufw allow 25565
+  sudo ufw enable
+}
 
-# Set Minecraft up as a service
-mv minecraft.service /etc/systemd/system/minecraft.service
-chmod +x /etc/systemd/system/minecraft.service
+# Create MC User
+createMCUserAndGroups() {
+  sudo useradd minecraft
+  sudo groupadd mc_server_admin
+  sudo usermod -aG mc_server_admin minecraft
+  sudo usermod -aG mc_server_admin $USER #adds current user to Minecraft admin group
+}
 
-# Download and install JDK 16
-curl -O https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_linux-x64_bin.tar.gz
-sudo tar xvf openjdk-16.0.2_linux-x64_bin.tar.gz
-sudo mv jdk-16.0.2 /opt/
-rm openjdk-16.0.2_linux-x64_bin.tar.gz
-# Add Java variables to admin bashrc
-echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
-echo "export PATH=$PATH:$JAVA_HOME/bin" >> ~/.bashrc
-# Add Java variables to Minecraft Service Account Bashrc
-echo "export JAVA_HOME=/opt/jdk-16.0.2" >> $MC_USER_HOME_DIR/.bashrc
-echo "export PATH=$PATH:$JAVA_HOME/bin" >> $MC_USER_HOME_DIR/.bashrc
-. ~/.bashrc
+# Install Java
+installJava() {
+  curl -O https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_linux-x64_bin.tar.gz
+  sudo tar xvf openjdk-16.0.2_linux-x64_bin.tar.gz
+  sudo mv jdk-16.0.2 /opt/
+  rm openjdk-16.0.2_linux-x64_bin.tar.gz
+  # Add Java variables to admin bashrc
+  echo "export JAVA_HOME=$JAVA_HOME" >>~/.bashrc
+  echo "export PATH=$PATH:$JAVA_HOME/bin" >>~/.bashrc
+  # Add Java variables to Minecraft Service Account Bashrc
+  echo "export JAVA_HOME=/opt/jdk-16.0.2" >>$MC_USER_HOME_DIR/.bashrc
+  echo "export PATH=$PATH:$JAVA_HOME/bin" >>$MC_USER_HOME_DIR/.bashrc
+  . ~/.bashrc
 
-# Setup server directories
-sudo mkdir $MC_SERVER_DIR
-sudo wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -P $MC_SERVER_DIR
+}
 
-# Auto accept Minecraft EULA
-echo "eula=true" > $MC_SERVER_DIR/eula.txt
+# Create and configure the MC server direcotyr
+setupMCServerDirectory() {
+  sudo mkdir $MC_SERVER_DIR
+  mv ~/Setup_Minecraft_Server/start_minecraft_server.sh start_minecraft_server.sh
+  chmod +x start_mincraft_server.sh
+}
 
-# Move the start script 
-mv ~/Setup_Minecraft_Server/start_minecraft_server.sh start_minecraft_server.sh
-chmod +x start_mincraft_server.sh
+# Install MC Server 1.17.1
+installMCServer() {
+  sudo wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -P $MC_SERVER_DIR
+  echo "eula=true" >$MC_SERVER_DIR/eula.txt
+  sudo chown -R root:mc_server_admin $MC_SERVER_DIR
+  sudo chmod 770 -R $MC_SERVER_DIR
+}
 
-# Fix file permissions
-sudo chown -R root:mc_server_admin $MC_SERVER_DIR
-sudo chmod 770 -R $MC_SERVER_DIR
+# Install and start MC Service
+installAndStartMCService() {
+  mv minecraft.service /etc/systemd/system/minecraft.service
+  chmod +x /etc/systemd/system/minecraft.service
+  sudo systemctl daemon-reload
+  sudo systemctl enable minecraft.service
+  sudo systemctl start minecraft.service
+}
 
-# Update services and start MC server
-sudo systemctl daemon-reload
-sudo systemctl enable minecraft.service
-sudo systemctl start minecraft.service
+test() {
+  echo "This is a test function"
+}
+
+##############################################
+# Logic to determine which functions to call #
+##############################################
+
+if [[ "$UNAME" == "Darwin" ]]; then
+  echo "You are on a Mac. This process is not yet supported on MacOS"
+  $test
+else
+  ISSUE_FILE=$(cat /etc/issue)
+  if [[ "$ISSUE_FILE" == *"Ubuntu"* ]]; then
+    echo "You are on Ubuntu"
+  else
+    RHEL_VERSION=$(hostnamectl | grep "Operating System")
+    if [[ "$RHEL_VERSION" == *"Oracle"* ]]; then
+      echo "You are on Oracle Linux"
+      $installPackagesForOracleLinux
+    elif [["$RHEL_VERSION" == *"CentOS"*]]; then
+      echo "You are on Oracle Linux"
+    else
+      echo "Unfortunately, this script is not setup for $RHEL_VERSION"
+    fi
+  fi
+  else
+  echo "something else"
+fi
+
+# # Fix file permissions
