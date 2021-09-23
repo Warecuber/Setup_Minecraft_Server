e4ff5bd54262a1f0b7e11de4f03cea3317e6b388
John W <56055207+Warecuber@users.noreply.github.com>
2021-09-22 21:55:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # variables
 $MC_USER_HOME_DIR="/home/minecraft"
-$MC_SERVER_DIR="/opt/minecraft"
+$MC_SERVER_DIR="/opt/minecraft/server"
 $JAVA_HOME="/opt/jdk-16.0.2"
 
 # Install packages
@@ -37,18 +37,21 @@ echo "export PATH=$PATH:$JAVA_HOME/bin" >> $MC_USER_HOME_DIR/.bashrc
 . ~/.bashrc
 
 # Setup server directories
-sudo mkdir $MC_SERVER_DIR/server
-sudo chown -R root:mc_server_admin $MC_SERVER_DIR/server
-sudo chmod 770 -R $MC_SERVER_DIR/server
-sudo wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -P $MC_SERVER_DIR/server
+sudo mkdir $MC_SERVER_DIR
+sudo wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -P $MC_SERVER_DIR
 
 # Auto accept Minecraft EULA
-echo "eula=true" > $MC_SERVER_DIR/server/eula.txt
+echo "eula=true" > $MC_SERVER_DIR/eula.txt
 
 # Move the start script 
 mv ~/Setup_Minecraft_Server/start_minecraft_server.sh start_minecraft_server.sh
 chmod +x start_mincraft_server.sh
 
+# Fix file permissions
+sudo chown -R root:mc_server_admin $MC_SERVER_DIR
+sudo chmod 770 -R $MC_SERVER_DIR
+
+# Update services and start MC server
 sudo systemctl daemon-reload
 sudo systemctl enable minecraft.service
 sudo systemctl start minecraft.service
